0bfc28f0973d656a21f12f8f3c222b47d7970025
leo <wyonghao@outlook.com>
2023-11-07 15:42:25 +0000

    add week8 gpt code no working

---
+++ week8code/Pytsk3Ex_1.py
@@ -0,0 +1,40 @@
+'''
+The following skeleton code is designed to help you with the structure of the practice program
+for the pytsk Workshop 1
+As always, if you feel like something does not make sense, ask for help from your tutor, or
+read the relevant documentation.
+'''
+
+# 1 Import the required Library
+
+
+# 2 Create a new IMG_Info object by opening the file and name object as diskimage
+
+
+# 3 Display the size of the disk image in raw bytes
+
+
+# 4 closes the disk image much like the close method unlinked a file from a program using Python File I/O.
+
+
+# 5 Display the number of sectors there are in the disk image file
+
+
+# 6 Create a Volume_Info object for the Img_Info object to get a Volume_Info object containing the partitions defined in the disk image
+
+
+# 7 Displays the number of partitions pytsk3 has identified in the disk image.
+
+
+# 8 Defines the start of a for loop for processing each partition in the disk image.
+
+
+    # 9 displays a description of the partition’s type, start sector and number of sectors in the partition
+
+    # Convert the bytes into a string for volume.desc
+
+
+    # 11 This statement adds the current volume’s start sector to the end of the list each iteration
+
+
+# 12 Display the contents of the partitions list
---
+++ week8code/gptcode.py
@@ -0,0 +1,25 @@
+import pytsk3
+
+# Replace 'disk_image_path' with the path to your disk image file.
+disk_image_path = 'diskimageMT.001'
+
+# Open the disk image using pytsk3.
+img_info = pytsk3.Img_Info(disk_image_path)
+
+# Create a TSK filesystem object for the disk image.
+fs_info = pytsk3.FS_Info(img_info)
+
+# Get the list of partitions on the disk image.
+partitions = fs_info.info.meta
+
+# Print information about each partition.
+for partition in partitions:
+    print("Partition ID: {}".format(partition.addr))
+    print("Description: {}".format(partition.desc.decode('utf-8')))
+    print("Offset (in sectors): {}".format(partition.start))
+    print("Length (in sectors): {}".format(partition.len))
+    print("File System Type: {}".format(partition.fstype))
+    print()
+
+# Close the disk image.
+fs_info.close()
